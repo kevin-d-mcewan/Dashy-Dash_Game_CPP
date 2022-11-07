cf7ddfb24323bf50cf2d250899558e98f28fd20f
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2022-11-06 18:27:01 -0800

    added rectangle player and ability to jump simply

--- vscode-template-main/dasher.cpp
+++ vscode-template-main/dasher.cpp
@@ -3,11 +3,20 @@
 int main(int argc, char const *argv[])
 {
   //  Game Window Dimensions
-  const int window_width{960};
-  const int window_height{540};
+  const int windowWidth{960};
+  const int windowHeight{540};
 
   // Initialize the Game Window 
-  InitWindow(window_width, window_height, "Dashy Dash");
+  InitWindow(windowWidth, windowHeight, "Dashy Dash");
+
+  // Rectangle Dimensions (player)
+  const int width{50};
+  const int height{80};
+
+  //  y-pos for Rectangle (be placed on "ground")  must minus height from char bc total dimension of window is full pix of 540
+  int posY{windowHeight - height};
+  // to fly upward must go in the negative direction from og start point
+  int velocity{0}; 
 
   // Game Variables
   SetTargetFPS(60);
@@ -19,8 +28,20 @@ int main(int argc, char const *argv[])
       BeginDrawing();
       
       ClearBackground(WHITE);
+
       
+
+      if (IsKeyPressed(KEY_SPACE))
+      {
+        velocity -= 10;
+        
+      }
       
+
+      posY += velocity;
+      DrawRectangle(windowWidth/2, posY, width, height, BLUE);
+      
+
       EndDrawing();
       // Game Logic Ends
     }
