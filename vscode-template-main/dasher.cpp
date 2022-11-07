05caac2b0000178f7aa12975da6a757b6df5f973
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2022-11-06 20:23:16 -0800

    Added velocity and acceleration to gravity plus added a groundcheck

--- vscode-template-main/dasher.cpp
+++ vscode-template-main/dasher.cpp
@@ -9,14 +9,21 @@ int main(int argc, char const *argv[])
   // Initialize the Game Window 
   InitWindow(windowWidth, windowHeight, "Dashy Dash");
 
+  // acceleration due to gravity (pixels/frame)/frame
+  const int gravity{1};
+  const int jumpVelo{-18};
+
   // Rectangle Dimensions (player)
   const int width{50};
   const int height{80};
 
   //  y-pos for Rectangle (be placed on "ground")  must minus height from char bc total dimension of window is full pix of 540
   int posY{windowHeight - height};
-  // to fly upward must go in the negative direction from og start point
+  // to fly upward must go in the negative direction from og start point THIS IS INTIALIZE  VELO
   int velocity{0}; 
+
+  // Check to see if already in air.
+  bool isInAir{false};
 
   // Game Variables
   SetTargetFPS(60);
@@ -29,15 +36,36 @@ int main(int argc, char const *argv[])
       
       ClearBackground(WHITE);
 
-      
 
-      if (IsKeyPressed(KEY_SPACE))
+      // ground check
+      if (posY >= windowHeight - height)
       {
-        velocity -= 10;
+        /* Rectangle is on ground */
+        
+          isInAir = false;
+         velocity = 0;
+      }
+      else
+      {
+        // Rectangle is in air 
+        // Apply gravity.
+        velocity += gravity;
+        isInAir = true;
+      }
+
+      // Check for Jumping
+      if (IsKeyPressed(KEY_SPACE) && !isInAir)
+      {
+        velocity += jumpVelo;
         
       }
       
+      
 
+      // apply gravity
+      
+
+      // Update Velocity
       posY += velocity;
       DrawRectangle(windowWidth/2, posY, width, height, BLUE);
       
